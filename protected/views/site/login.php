b9a22ddf99bd06b591daac4d81f7334de874e767
phongsathon-jk <phongsathon.th@gmail.com>
2015-07-27 15:55:15 +0700

    try to ignore index.php

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -12,7 +12,7 @@ echo TbHtml::breadcrumbs(array(
 
 <h1>Login</h1>
 
-<p>Please fill out the following form with your login credentials:</p>
+<p>555 Please fill out the following form with your login credentials:</p>
 
 <div class="form">
 <?php $form=$this->beginWidget('CActiveForm', array(
