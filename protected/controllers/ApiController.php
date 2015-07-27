3382b034552bfd6aa90addd1e9f46c255e98d28a
phongsathon-jk <phongsathon.th@gmail.com>
2015-07-28 01:20:21 +0700

    complete search with keyword API

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -9,9 +9,32 @@ class ApiController extends Controller {
     // Actions
     public function actionList() {
         // get the data
-        $models = Place::model()->findAll();
+        $places = Place::model()->findAll();
         $rows = array();
-        foreach($models as $model) {
+        foreach($places as $model) {
+            $rows[] = array(
+                'id' => $model->id,
+                'type' => $model->type,
+                'name' => $model->name,
+                'detail' => $model->detail,
+                'pic' => $model->pic,
+            );
+        }
+        // Send the response
+        echo json_encode($rows);
+    }
+
+    public function actionSearch($keyword) {
+        // get the data
+        $keyword = addcslashes($keyword, '%_'); // escape LIKE's special characters
+        $q = new CDbCriteria( array(
+            'condition' => "name LIKE :keyword",         // no quotes around :match
+            'params'    => array(':keyword' => "%$keyword%")  // Aha! Wildcards go here
+        ) );
+
+        $places = Place::model()->findAll($q);
+        $rows = array();
+        foreach($places as $model) {
             $rows[] = array(
                 'id' => $model->id,
                 'type' => $model->type,
