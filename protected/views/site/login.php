7f5dff2a173ef4d62be8ad597032dbea147ac3e9
phongsathon-jk <phongsathon.th@gmail.com>
2015-07-27 16:11:37 +0700

    begin the project

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -12,7 +12,7 @@ echo TbHtml::breadcrumbs(array(
 
 <h1>Login</h1>
 
-<p>555 Please fill out the following form with your login credentials:</p>
+<p>Please fill out the following form with your login credentials:</p>
 
 <div class="form">
 <?php $form=$this->beginWidget('CActiveForm', array(
