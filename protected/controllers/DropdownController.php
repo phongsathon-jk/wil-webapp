89b8774d0d2ec46efab5595f60a0acad3f874785
KalumDog <laonapaporn_22@hotmail.com>
2015-07-29 00:37:40 +0700

    Dropdown

---
+++ protected/controllers/DropdownController.php
@@ -0,0 +1,100 @@
+<?php
+
+class DropdownController extends Controller
+{
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+	
+	
+	// Uncomment the following methods and override them if needed
+	
+	 public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array();
+	} 
+
+	public function actionDynamic()
+	{
+		if($_GET['category_id'] == 0){ 
+			$rows = array();
+			$places = Place::model()->findAll();
+			 foreach($places as $place) {
+			$rows[] = array('name'=>$place->name,
+							'category'=>0);
+			
+		   //$rows[] = "ITEM"=>array("place"=>$place->name),"Category"=>array('id'=>$_GET['category_id']);
+			}
+            
+           // foreach ($rows as $value) {
+                //echo CHtml::tag('option', array('value' => $value),
+                //CHtml::encode('' . $value), true);
+            //}
+			
+			//array_push($rows,$_GET['category_id']);
+			
+			echo json_encode($rows);
+			 
+			//echo json_encode($_GET['category_id']);
+			//exit();
+			//$this->render('index',$rows[]);
+			return $_GET['category_id'];
+        }
+		
+		if($_GET['category_id'] == 1){ 
+			$rows = array();
+			 $places = Place::model()->findAllByAttributes(array('type' => "temple"));
+			 foreach($places as $place) {
+            $rows[] = $place->name;
+			
+			}
+           
+            /* foreach ($rows as $value) {
+                echo CHtml::tag('option', array('value' => $value),
+                CHtml::encode('' . $value), true);
+            } */
+			array_push($rows,$_GET['category_id']);
+			 		
+			echo json_encode($rows);
+			//exit();
+			//$this->render('index',$rows[]);	
+			
+        }
+       
+        if($_GET['category_id'] == 2){
+			$rows = array();
+			$places = Place::model()->findAllByAttributes(array('type' => 'natural'));
+             foreach($places as $place) {
+            $rows[] = $place->name;
+			}
+            
+            // foreach ($rows as $value) {
+                // echo CHtml::tag('option', array('value' => $value),
+                // CHtml::encode('' . $value), true);
+            // }
+			array_push($rows,$_GET['category_id']);
+			echo json_encode($rows);
+        }
+     
+        if($_GET['category_id'] == 3){
+			$rows = array();
+			 $places = Place::model()->findAllByAttributes(array('type' => 'culture'));
+             foreach($places as $place) {
+            $rows[] = $place->name;
+			}
+            
+            // foreach ($rows as $value) {
+                // echo CHtml::tag('option', array('value' => $value),
+                // CHtml::encode('' . $value), true);
+            // }
+			array_push($rows,$_GET['category_id']);
+			echo json_encode($rows);
+        }
+       
+         
+        exit();
+	}
+	
+}
---
+++ protected/views/dropdown/index.php
@@ -0,0 +1,73 @@
+
+<?php
+/* @var $this DropdownController */
+
+$this->breadcrumbs=array(
+	'Dropdown',
+);
+$this->pageTitle = Yii::app()->name;
+echo CHtml::beginForm();
+    echo CHtml::label('Category ', false);
+    echo CHtml::dropDownList('category_id', '',
+        array(
+            0 => 'select category',
+            1 => 'Temple',
+            2 => 'Natural',
+            3 => 'Culture',
+            
+        ),
+        array(
+            'ajax' => array(
+                'type' => 'GET',
+                'url' => $this->createUrl('dropdown/dynamic'),
+                'update' => '#place_id',
+				//'id' => 'c_id',
+            )
+        )
+    );
+    echo CHtml::label(' Place ', false);
+    echo CHtml::dropDownList('place_id', '', array('' => 'select place'));
+echo CHtml::endForm();
+?>
+<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+
+<p>
+	You may change the content of this page by modifying
+	the file <tt><?php echo __FILE__; ?></tt>.
+</p>
+<div id="test">
+	
+	
+</div>
+	<script>
+		/* function autoList(c_id){
+			
+		} */
+		$(document).ready(function(){
+		$.ajax({
+		url: '/wil_webapp/dropdown/dynamic',
+		method: "GET",
+		dataType: "json",
+		data:{
+			"category_id": 0
+		}
+		}).done(function(data){
+			$("#test").html(''); 
+			$.each(data, function(data, value) {
+				
+			$('#test').append(value.name);
+			$('#test').append('<br>');
+			
+			
+		});
+			
+			//$('#test').append(data.name);
+		//data->name
+		//alert(data);
+	});
+		
+});
+		/* $("#category_id").click(function() {
+		autoList($("#keyword").val());
+	}); */
+	</script>
