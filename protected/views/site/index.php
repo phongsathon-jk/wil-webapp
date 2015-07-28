08fd949f7fb230c066b7e5e2cd9e049fb38c1b2f
phongsathon-jk <phongsathon.th@gmail.com>
2015-07-28 11:44:35 +0700

    use openweather API. show it on homepage.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,7 +4,6 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 <h1>Attractions in Chiang Mai</h1>
-
 <div class="row" id="search_option">
 	<div class="col-md-6 col-md-offset-3">
 		<div class="row">
@@ -13,6 +12,11 @@ $this->pageTitle=Yii::app()->name;
 			</div>
 			<div class="col-md-6" id="autocomplete_ajax">
 				autocomplete
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12" style="text-align: center;">
+				<a href="#">check-out the restaurants</a>
 			</div>
 		</div>
 	</div>
@@ -26,23 +30,30 @@ $this->pageTitle=Yii::app()->name;
 		<?php
 		$places = Place::model()->findAll();
 		foreach($places as $place):
-		?>
-		<div class="row" id="single_place">
-			<div class="col-md-4">
-				<a href="<?php echo Yii::app()->request->url; ?>view?id=<?php echo $place->id; ?>">
-					<img class="img-responsive img-thumbnail" src="<?php //echo $place->pic; ?>" alt="<?php echo $place->name; ?>">
-				</a>
+			?>
+			<div class="row" id="single_place">
+				<div class="col-md-4">
+					<a href="<?php echo Yii::app()->request->url; ?>view?id=<?php echo $place->id; ?>">
+						<img class="img-responsive img-thumbnail" src="<?php //echo $place->pic; ?>" alt="<?php echo $place->name; ?>">
+					</a>
+				</div>
+				<div class="col-md-8" id="short_detail">
+					<a href="<?php echo Yii::app()->request->url; ?>view?id=<?php echo $place->id; ?>">
+						<label><?php echo $place->name; ?></label>
+					</a>
+					<p><?php echo $place->detail; ?></p>
+				</div>
 			</div>
-			<div class="col-md-8" id="short_detail">
-				<a href="<?php echo Yii::app()->request->url; ?>view?id=<?php echo $place->id; ?>">
-					<label><?php echo $place->name; ?></label>
-				</a>
-				<p><?php echo $place->detail; ?></p>
-			</div>
-		</div>
 		<?php endforeach; ?>
 	</div>
 	<div class="col-md-2" id="weather">
-		weather api
+		<?php
+		$weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=chiangmai&units=metric"));
+		?>
+		<img src="http://openweathermap.org/img/w/<?php echo $weather->weather[0]->icon; ?>.png" alt="<?php echo $weather->weather[0]->main; ?>">
+		<p>Condition: <?php echo $weather->weather[0]->main; ?></p>
+		<p>Temperature: <?php echo $weather->main->temp; ?> &#8451;</p>
+		<p>Min. Temp: <?php echo $weather->main->temp_min; ?> &#8451;</p>
+		<p>Max. Temp: <?php echo $weather->main->temp_max; ?> &#8451;</p>
 	</div>
 </div>
