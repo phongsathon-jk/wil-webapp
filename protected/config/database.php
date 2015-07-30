920c6a0461c046ba7b82df4628b67e17309c884d
KalumDog <laonapaporn_22@hotmail.com>
2015-07-30 10:40:36 +0700

    by oat

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,6 +5,6 @@ return array(
 	'connectionString' => 'mysql:host=localhost;dbname=wil_webapp',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => 'root',
 	'charset' => 'utf8'
 );
